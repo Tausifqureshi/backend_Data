17f6b04dc9b7ccda8a50220d4ff4d711f4c5df98
Tausifqureshi <tausifqureshi504@gmail.com>
2024-05-10 01:56:04 +0530

    inital

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import jsonServer from "json-server";
-// const jsonServer = require("json-server");
+// import jsonServer from "json-server";
+const jsonServer = require("json-server");
 // const jsonServer = require("json-server")
 const server = jsonServer.create();
 // const router = jsonServer.router("db.json");
